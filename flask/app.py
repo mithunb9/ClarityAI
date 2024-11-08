b958e9234feaba7c75dda04dfd73a321316708e3
Mithun Balasubramanian <sendmithunb@gmail.com>
2024-11-07 18:05:11 -0600

    add cors support and add flask pdf route support

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from pdf import extract_pdf_text_from_s3
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -9,11 +11,17 @@ def home():
 
 @app.route('/extract', methods=['POST'])
 def extract():
-    data = request.json
-    file_key = data.get('file_key')
-    text = extract_pdf_text_from_s3(file_key)
-    
-    return jsonify({'text': text})
+    try:
+        data = request.json
+        if not data or 'file_key' not in data:
+            return jsonify({'error': 'No file_key provided'}), 400
+            
+        file_key = data.get('file_key')
+        text = extract_pdf_text_from_s3(file_key)
+        
+        return jsonify({'text': text})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
