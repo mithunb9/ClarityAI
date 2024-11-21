ecf7aaad5464c5442c0c8ecb33f4636502abf9da
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-11-21 09:00:33 -0600

    update navbar to have file and quiz view

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,14 @@
-import { Box, Flex, Text, Container } from "@chakra-ui/react";
+"use client";
+
+import { Box, Flex, Text, Container, Button, HStack } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 import AuthButton from "./AuthButton";
+import { useSession } from "next-auth/react";
 
 function Navbar() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   return (
     <Box bg="blue.500" py={2}>
       <Container maxW="container.xl">
@@ -15,10 +22,35 @@ function Navbar() {
             fontWeight="bold" 
             color="white"
             letterSpacing="tight"
+            cursor="pointer"
+            onClick={() => router.push("/")}
           >
             ClarityAI
           </Text>
-          <AuthButton />
+          
+          <HStack spacing={4}>
+            {session && (
+              <>
+                <Button
+                  variant="ghost"
+                  color="white"
+                  _hover={{ bg: 'blue.600' }}
+                  onClick={() => router.push("/upload")}
+                >
+                  Upload Files
+                </Button>
+                <Button
+                  variant="ghost"
+                  color="white"
+                  _hover={{ bg: 'blue.600' }}
+                  onClick={() => router.push("/quizzes")}
+                >
+                  My Quizzes
+                </Button>
+              </>
+            )}
+            <AuthButton />
+          </HStack>
         </Flex>
       </Container>
     </Box>
