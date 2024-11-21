bdfbb0f736bda57ceb765ec5d3bd3fe5efc98381
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-11-21 10:26:27 -0600

    update packages and example files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com', 'authjs.dev'],
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, 'canvas', 'jsdom'];
+    return config;
+  },
+};
 
 export default nextConfig;
