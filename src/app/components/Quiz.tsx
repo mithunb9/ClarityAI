9dbc7ff958b15fd1f528c929169e5487486952df
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-11-21 15:29:10 -0600

    create quiz components

--- src/app/components/Quiz.tsx
+++ src/app/components/Quiz.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import AnswerChoice from "./AnswerChoice";
-import { Box, Text, VStack } from "@chakra-ui/react";
+import { Box, Text, VStack, Container, Heading } from "@chakra-ui/react";
 
 interface QuizProps {
     quiz: {
@@ -16,25 +16,37 @@ interface QuizProps {
 
 const QuizComponent: FC<QuizProps> = ({ quiz }) => {
     return (
-        <VStack spacing={6} align="stretch">
-            {quiz.questions.map((question, questionIndex: number) => (
-                <Box key={questionIndex} p={4} borderWidth={1} borderRadius="lg">
-                    <Text fontSize="lg" fontWeight="bold" mb={4}>
-                        {questionIndex + 1}. {question.question}
-                    </Text>
-                    <VStack spacing={3} align="stretch">
-                        {question.answer_choices.map((choice, choiceIndex: number) => (
-                            <AnswerChoice
-                                key={choiceIndex}
-                                content={choice.content}
-                                correct={choice.correct}
-                                index={choiceIndex}
-                            />
-                        ))}
-                    </VStack>
-                </Box>
-            ))}
-        </VStack>
+        <Container maxW="container.lg" py={8}>
+            <VStack spacing={8} align="stretch">
+                <Heading size="xl" textAlign="center" mb={6}>
+                    My Questions
+                </Heading>
+                {quiz.questions.map((question, questionIndex) => (
+                    <Box 
+                        key={questionIndex} 
+                        p={6} 
+                        borderWidth={1} 
+                        borderRadius="lg" 
+                        boxShadow="sm"
+                        bg="white"
+                    >
+                        <Text fontSize="xl" fontWeight="bold" mb={4}>
+                            {questionIndex + 1}. {question.question}
+                        </Text>
+                        <VStack spacing={4} align="stretch">
+                            {question.answer_choices.map((choice, choiceIndex) => (
+                                <AnswerChoice
+                                    key={choiceIndex}
+                                    content={choice.content}
+                                    correct={choice.correct}
+                                    index={choiceIndex}
+                                />
+                            ))}
+                        </VStack>
+                    </Box>
+                ))}
+            </VStack>
+        </Container>
     );
 };
 
